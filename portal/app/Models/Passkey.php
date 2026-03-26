9cf1281efc5bf0f135c89f7d2ac7c011179db8a1
majikandy <github@andy-davis.co.uk>
2026-03-26 20:00:27 +0000

    Fix credential_source double-encoding: decode manually instead of array cast

--- portal/app/Models/Passkey.php
+++ portal/app/Models/Passkey.php
@@ -9,10 +9,10 @@ class Passkey extends Model
 {
     protected $fillable = ['user_id', 'name', 'credential_id', 'credential_source'];
 
-    protected $casts = ['credential_source' => 'array'];
-
     public function credentialSource(): PublicKeyCredentialSource
     {
-        return PublicKeyCredentialSource::createFromArray($this->credential_source);
+        return PublicKeyCredentialSource::createFromArray(
+            json_decode($this->credential_source, true)
+        );
     }
 }
